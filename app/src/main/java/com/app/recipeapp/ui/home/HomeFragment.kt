44985f8e31f70b0a7c1d916f9bca2b2cd8bd9180
GurpreetSudan <gurpreetsudan2094@gmail.com>
2023-12-16 22:24:37 +0530

    main recyclerview working

--- app/src/main/java/com/app/recipeapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/app/recipeapp/ui/home/HomeFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.app.recipeapp.R
 import com.app.recipeapp.databinding.FragmentHomeBinding
 import com.app.recipeapp.pojo.network.RecipeState
@@ -20,9 +21,20 @@ class HomeFragment : Fragment() {
 
     private var binding : FragmentHomeBinding? = null
     private val recipeViewModel: HomeViewModel by viewModels()
+    private lateinit var recipeAdapter: RecipeAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        recipeAdapter = RecipeAdapter()
+        binding?.verticalRecyclerview?.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = recipeAdapter
+        }
     }
 
     override fun onCreateView(
@@ -33,7 +45,7 @@ class HomeFragment : Fragment() {
         binding = DataBindingUtil
             .inflate<FragmentHomeBinding>(inflater, R.layout.fragment_home, container, false)
 
-        // Observe LiveData
+
         recipeViewModel.recipeState.observe(viewLifecycleOwner) { state ->
             binding?.progressBar?.visibility = if (state is RecipeState.Loading) View.VISIBLE else View.GONE
             when (state) {
@@ -42,16 +54,12 @@ class HomeFragment : Fragment() {
                 }
 
                 is RecipeState.Success -> {
-                    // Update UI with recipeSearchResponse
                     val recipeSearchResponse = state.recipeSearchResponse
-                    // Handle success UI state
-                    Log.d(TAG, "Received response: $recipeSearchResponse")
+                    recipeAdapter.updateData(recipeSearchResponse.hits)
                 }
 
                 is RecipeState.Error -> {
-                    // Show error message or handle error UI state
                     val errorMessage = state.errorMessage
-                    // Handle error UI state
                     Log.d(TAG, "Received error $state ${state.errorMessage}")
                 }
             }
@@ -60,4 +68,8 @@ class HomeFragment : Fragment() {
         return binding!!.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
 }
---
+++ app/src/main/java/com/app/recipeapp/ui/home/RecipeAdapter.kt
@@ -0,0 +1,48 @@
+package com.app.recipeapp.ui.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.app.recipeapp.databinding.RecyclerviewItemVerticalBinding
+import com.app.recipeapp.pojo.network.Hit
+import com.app.recipeapp.pojo.network.Recipe
+
+class RecipeAdapter : RecyclerView.Adapter<RecipeAdapter.RecipeViewHolder>() {
+
+    private var hits: List<Hit> = emptyList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding: RecyclerviewItemVerticalBinding =
+            RecyclerviewItemVerticalBinding.inflate(inflater, parent, false)
+        return RecipeViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: RecipeViewHolder, position: Int) {
+        val recipe = hits[position].recipe
+
+        if (recipe != null) {
+            holder.bind(recipe)
+        }
+
+
+    }
+
+    override fun getItemCount(): Int = hits.size
+
+    fun updateData(hitsList: List<Hit>?) {
+        if (hitsList != null) {
+            hits = hitsList
+            notifyDataSetChanged()
+        }
+    }
+
+    inner class RecipeViewHolder(private val binding: RecyclerviewItemVerticalBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(recipe: Recipe) {
+            binding.recipe = recipe
+            binding.executePendingBindings()
+        }
+    }
+}
